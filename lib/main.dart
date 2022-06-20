59a0cac82bb714f64abe890d2e3dfd3734a4065e
Muhammad Vaid <molvy32@gmail.com>
2022-06-20 10:01:22 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+
+import 'package:ecommerce_app/src/app.dart';
+import 'package:ecommerce_app/src/features/cart/data/local/local_cart_repository.dart';
+import 'package:ecommerce_app/src/features/cart/data/local/sembast_cart_repository.dart';
+import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+void main() async {
+  // * For more info on error handling, see:
+  // * https://docs.flutter.dev/testing/errors
+  await runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    // turn off the # in the URLs on the web
+    GoRouter.setUrlPathStrategy(UrlPathStrategy.path);
+
+    // Wait for repository to be created before running App!!
+    final LocalCartRepository = await SembastCartRepository.makeDefault();
+    // * Entry point of the app
+    runApp(ProviderScope(
+      overrides: [
+        localCartRepositoryProvider.overrideWithValue(LocalCartRepository),
+      ],
+      child: const MyApp(),
+    ));
+
+    // * This code will present some error UI if any uncaught exception happens
+    FlutterError.onError = (FlutterErrorDetails details) {
+      FlutterError.presentError(details);
+    };
+    ErrorWidget.builder = (FlutterErrorDetails details) {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text('An error occurred'.hardcoded),
+        ),
+        body: Center(child: Text(details.toString())),
+      );
+    };
+  }, (Object error, StackTrace stack) {
+    // * Log any errors to console
+    debugPrint(error.toString());
+  });
+}
---
+++ lib/src/features/cart/presentation/cart_total/cart_total_text.dart
@@ -0,0 +1,21 @@
+import 'package:ecommerce_app/src/utils/currency_formatter.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+/// Text widget for showing the total price of the cart
+class CartTotalText extends ConsumerWidget {
+  const CartTotalText({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // TODO: Read from data source
+    const cartTotal = 104.0;
+    final totalFormatted =
+        ref.watch(currencyFormatterProvider).format(cartTotal);
+    return Text(
+      'Total: $totalFormatted',
+      style: Theme.of(context).textTheme.headline5,
+      textAlign: TextAlign.center,
+    );
+  }
+}
